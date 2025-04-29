4e856830f68d9fe17bde70c44e67f6be47a43c1a
Jahat Trinidad <jahatjassiel@gmail.com>
2025-04-29 17:35:46 -0500

    Implementacion de las interfaces de la Landingpage y el Login

--- CourseraClone/CourseraCloneApp.cpp
+++ CourseraClone/CourseraCloneApp.cpp
@@ -1,10 +1,19 @@
 #include "iostream"
+#include "ExtendedFunctions.h"
+#include "UI_Ascii.h"
 
 using namespace std;
+using namespace System;
 
 void main()
 {
-	cout << "Coursera Clone App" << endl;
+	ConfigurarConsola();
 
+	system("cls");
+	UI_LandingPage();
+	system("pause");
+
+	system("cls");
+	UI_Login();
 	system("pause");
 }
---
+++ CourseraClone/ExtendedFunctions.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "iostream"
+#include "windows.h" // acceder a la api de windows
+
+using namespace std;
+using namespace System;
+
+// Configurar la fuente y tamano de la letra
+void CambiarFuenteConsola(const wstring& nombreFuente, COORD tamanioFuente) {
+    HANDLE hConsola = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_FONT_INFOEX cfi = { 0 };
+    cfi.cbSize = sizeof(cfi);
+    wcscpy_s(cfi.FaceName, nombreFuente.c_str());
+    cfi.dwFontSize = tamanioFuente;
+    cfi.FontWeight = FW_NORMAL;
+
+    SetCurrentConsoleFontEx(hConsola, FALSE, &cfi);
+}
+
+// Configurar la consola
+void ConfigurarConsola()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+
+	CambiarFuenteConsola(L"Cascadia Mono", { 0, 24 });
+
+	Console::BackgroundColor = ConsoleColor::White;
+	Console::ForegroundColor = ConsoleColor::Black;
+
+	Console::SetWindowSize(120, 35);
+
+	SetConsoleTitle(L"Coursera Clone | Alpha 1");
+}
